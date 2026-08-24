909bdc72e82d6fbf0b982820be7943347e44dd9b
Jonathan Alejandro Rodríguez López <itsjhonalex@gmail.com>
2026-08-24 16:09:54 -0400

    🗑️ Eliminación de archivos obsoletos y limpieza del proyecto
    - Eliminados archivos de configuración y documentación no utilizados, incluyendo CHANGELOG.md, CODE_OF_CONDUCT.md, CONTRIBUTING.md, LICENSE.md, y otros.
    - Actualización del archivo .gitignore para incluir la carpeta 'legacy/'.
    
    Esta limpieza mejora la organización del proyecto y elimina elementos innecesarios. 🚀

--- update_database.py
+++
@@ -1,9 +0,0 @@
-from src.utils.database import Base, engine, create_tables
-from src.utils.logger import Logger
-
-def update_database():
-    create_tables()
-    Logger.success("Base de datos actualizada con éxito.")
-
-if __name__ == "__main__":
-    update_database()
